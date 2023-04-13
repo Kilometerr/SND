1038b99ec728930bc899db425e4400fc3dce6ca1
Tymoteusz Nowicki <tymoteusz.nowicki@capgemini.com>
2023-04-13 12:15:49 +0200

    added Item backend model, cleaned template

---
+++ Snd API/Models/Item.cs
@@ -0,0 +1,11 @@
+namespace Snd_API.Models
+{
+    public class Item
+    {
+        public Guid Id {get; set;}
+        public int GameId {get; set;}
+        public string Name {get; set;}
+        public int SoldToday {get; set;}
+        public int SoldWeek {get; set;}
+    }
+}
