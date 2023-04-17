44c36088271d1600f229abc9bc9b48b25a651139
Tymoteusz Nowicki <tymoteusz.nowicki@capgemini.com>
2023-04-17 10:12:28 +0200

    outfit ids can now be viewed through anular app

--- SndAPI/Controllers/ItemController.cs
+++ SndAPI/Controllers/ItemController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SndAPI.Clients;
 using SndAPI.Services;
+using SndAPI.Data;
 
 namespace SndAPI.Controllers
 {
@@ -10,21 +12,21 @@ namespace SndAPI.Controllers
     {
         private readonly IArshaService _arshaService;
         private readonly IBdoApiClient _bdoApiClient;
+        private readonly SndDbContext _sndDbContext;
 
-        public ItemController(IArshaService arshaService, IBdoApiClient bdoApiClient)
+        public ItemController(IArshaService arshaService, IBdoApiClient bdoApiClient, SndDbContext sndDbContext)
         {
             _arshaService = arshaService;
             _bdoApiClient = bdoApiClient;
+            _sndDbContext = sndDbContext;
         }
 
         //todo add so it gets id from form on site
         [HttpGet]
-        public async Task<IActionResult> GetList()
+        public async Task<IActionResult> GetList() //controller for tests
         {
-            HttpClient client = _bdoApiClient.GetClientList();//replace with database call
-            //add business logic to get data for specific item
-            var result = await _arshaService.GetById(client, 10006);
-            return Ok(result);
+            var Ids = await _sndDbContext.OutfitIDs.ToListAsync();
+            return Ok(Ids);
         }
     }
 }
--- SndUI/Snd.UI/src/app/app.module.ts
+++ SndUI/Snd.UI/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsListComponent } from './components/Items/items-list/items-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ItemsListComponent } from './components/Items/items-list/items-list.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
